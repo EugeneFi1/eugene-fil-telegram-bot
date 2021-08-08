ab40d9949c3095bfc11694b72b45220d7183c885
Eugene <jekafil@gmail.com>
2021-08-08 13:26:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+import {token} from "./utils/const";
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, async (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+    const chatId = msg['chat'].id;
+    const resp = match[1]; // the captured "whatever"
+    // send back the matched "whatever" to the chat
+    await bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', async (msg) => {
+    const chatId = msg['chat'].id;
+    console.log(chatId);
+    await bot.sendMessage(chatId, 'Received your message');
+});
